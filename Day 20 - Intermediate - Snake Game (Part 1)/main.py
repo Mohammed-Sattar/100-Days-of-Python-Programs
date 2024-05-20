e064b514029fe96bb1911fb76fbaa71d13056bc9
Mohammed-Sattar <mohammed.sattar1437@gmail.com>
2024-05-20 10:56:27 +0300

    27th commit 'Completed day 20 code (Snake Game Part 1)'

---
+++ Day 20 - Intermediate - Snake Game (Part 1)/main.py
@@ -0,0 +1,30 @@
+import time
+from turtle import Turtle, Screen
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0) # turns off the automatic updating of turtle's state
+
+is_game_on = False
+
+snake = Snake()
+screen.update()
+
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    snake.move()
+
+
+
+
+screen.exitonclick()
---
+++ Day 20 - Intermediate - Snake Game (Part 1)/snake.py
@@ -0,0 +1,64 @@
+from turtle import Turtle
+import time
+
+START_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.body = []
+        self.create_snake()
+    
+    def create_snake(self):
+        start_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+        self.segment1 = Turtle(shape="square")
+        self.segment2 = Turtle(shape="square")
+        self.segment3 = Turtle(shape="square")
+        self.segments = [self.segment1, self.segment2, self.segment3]
+
+        # segment (turtle) characteristics
+        for segment in self.segments:
+            segment.color("white")
+            segment.penup()
+            segment.speed(1)
+
+        for i in range(len(START_POSITIONS)):
+            self.segments[i].goto(START_POSITIONS[i])
+
+
+    def move(self):
+        time.sleep(0.1)
+
+        for segment in range(len(self.segments)-1, 0, -1):
+            new_pos = self.segments[segment - 1].position()
+            self.segments[segment].goto(new_pos)
+
+        self.segment1.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.segment1.heading() != DOWN:
+            self.segment1.setheading(UP)
+
+    def down(self):
+        if self.segment1.heading() != UP:
+            self.segment1.setheading(DOWN)
+        
+    def left(self):
+        if self.segment1.heading() != RIGHT:
+            self.segment1.setheading(LEFT)
+
+    def right(self):
+        if self.segment1.heading() != LEFT:
+            self.segment1.setheading(RIGHT)
+
+
+
+
+    def get_pos(self):
+        return self.segment1.position()
